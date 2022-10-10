ddaa5ae1c083867600f356b634c2b34b3fca3118
MOHAMMAD IQBAL MAULANA <iqbalmaulana99826@gmail.com>
2022-10-10 05:33:16 -0400

    perubahan pertama

---
+++ src/tabel/tablesiswa.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CButton,
+} from '@coreui/react'
+
+const Tablesiswa = () => {
+  return (
+    <div>
+      <CTable>
+        <CTableHead>
+          <CTableRow>
+            <CTableHeaderCell scope="col">ID</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Class</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+          </CTableRow>
+        </CTableHead>
+        <CTableBody>
+          <CTableRow active>
+            <CTableHeaderCell scope="row">1</CTableHeaderCell>
+            <CTableDataCell>Mark</CTableDataCell>
+            <CTableDataCell>Otto</CTableDataCell>
+            <CTableDataCell>@mdo</CTableDataCell>
+            <CButton color="primary" disabled>
+              Primary
+            </CButton>
+          </CTableRow>
+          <CTableRow>
+            <CTableHeaderCell scope="row">2</CTableHeaderCell>
+            <CTableDataCell>Jacob</CTableDataCell>
+            <CTableDataCell>Thornton</CTableDataCell>
+            <CTableDataCell>@fat</CTableDataCell>
+            <CButton color="primary" disabled>
+              Primary
+            </CButton>
+          </CTableRow>
+          <CTableRow active>
+            <CTableHeaderCell scope="row">3</CTableHeaderCell>
+            <CTableDataCell>Mark</CTableDataCell>
+            <CTableDataCell>Otto</CTableDataCell>
+            <CTableDataCell>@mdo</CTableDataCell>
+            <CButton color="primary" disabled>
+              Primary
+            </CButton>
+          </CTableRow>
+          <CTableRow>
+            <CTableHeaderCell scope="row">4</CTableHeaderCell>
+            <CTableDataCell>Jacob</CTableDataCell>
+            <CTableDataCell>Thornton</CTableDataCell>
+            <CTableDataCell>@fat</CTableDataCell>
+            <CButton color="primary" disabled>
+              Primary
+            </CButton>
+          </CTableRow>
+        </CTableBody>
+      </CTable>
+    </div>
+  )
+}
+
+export default Tablesiswa
